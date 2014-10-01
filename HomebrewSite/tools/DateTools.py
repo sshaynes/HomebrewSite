0442fbf0b615ed2ad4e02d9d9a054d24f386aec1
drew <drew.b.holland@gmail.com>
2014-09-30 22:52:07 -0700

    Added a serializer for datetime to ISO8601
    Added a serializer for datetime to ISO8601

---
+++ HomebrewSite/tools/DateTools.py
@@ -0,0 +1,11 @@
+import json
+import datetime
+import dateutil.parser
+class DateTools:
+	#takes in python datetime, returns a string in ISO 8601 format	
+	def datetimeToString(date):
+		return date.isoformat()
+		
+	#returns right now as an ISO 8601 string
+	def getNowAsString():
+		return datetime.datetime.now().isoformat()
--- HomebrewSite/views/auth/LoginView.py
+++ HomebrewSite/views/auth/LoginView.py
@@ -2,8 +2,14 @@ from django.http import HttpResponse
 from django.utils.html import escape
 from django.views.generic import View
 from django.contrib.sessions.backends.db import SessionStore
+from HomebrewSite.tools.DateTools import DateTools
 import json
 import datetime
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 class LoginView(View):
 
@@ -11,13 +17,14 @@ class LoginView(View):
 
 		#pending successful login
 		s = SessionStore()
-		s['last_login'] = datetime.datetime.now()
+		s['last_login'] = DateTools.getNowAsString()
 		s['userid'] = 1	#this should be set to the user id returned from authentication
 		s.save()
 		response = s.session_key
 		response += '_GET'
+		logger.info(DateTools.stringToDatetime(s['last_login']))
 
-		response = repr(request.GET)
+		#response = repr(request.GET)
 		# response = json.loads(request.GET)
 
 		response = HttpResponse(response)
@@ -28,7 +35,7 @@ class LoginView(View):
 
 		#pending successful login
 		s = SessionStore()
-		s['last_login'] = datetime.datetime.now()
+		s['last_login'] = DateTools.getNowAsString()
 		s['userid'] = 1	#this should be set to the user id returned from authentication
 		s.save()
 		response = s.session_key
