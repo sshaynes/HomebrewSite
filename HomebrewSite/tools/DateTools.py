d48beb21a86abb537751fe532cbde2b0562972e1
Zdravko Tyankov <ztyankov@gmail.com>
2014-10-01 22:40:44 -0400

    Made all DateTools methods static.

--- HomebrewSite/tools/DateTools.py
+++ HomebrewSite/tools/DateTools.py
@@ -1,10 +1,13 @@
 import json
 import datetime
+
 class DateTools:
-	#takes in python datetime, returns a string in ISO 8601 format	
+	#takes in python datetime, returns a string in ISO 8601 format
+	@staticmethod
 	def datetimeToString(date):
 		return date.isoformat()
-		
+
 	#returns right now as an ISO 8601 string
+	@staticmethod
 	def getNowAsString():
 		return datetime.datetime.now().isoformat()
