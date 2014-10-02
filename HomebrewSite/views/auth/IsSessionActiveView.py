8b8d53cb4c93c35e10fa8c1fce41880952f56bb4
sshaynes <sshaynes@gmail.com>
2014-10-01 21:23:45 -0400

    Working sessions
    is session active is working!

--- HomebrewSite/views/auth/IsSessionActiveView.py
+++ HomebrewSite/views/auth/IsSessionActiveView.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 from django.views.generic import View
 from django.contrib.sessions.backends.db import SessionStore
+import logging
 
 class IsSessionActiveView(View):
 
     def get(self, request, *args, **kwargs):
         try:
-            SessionStore(request["sessionID"])
-        except KeyError:
+            s = SessionStore(request.GET["sessionID"])
+            if s['userid'] != "":
+                return HttpResponse('1')
+            else:
+                return HttpResponse('0')
+        except:# KeyError:
             return HttpResponse('0')
-        return HttpResponse('1')
